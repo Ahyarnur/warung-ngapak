e7b104c9d6591cd341fc3623e34c35aae832b305
Ahyarnur <ahyarahyar5741@gmail.com>
2024-11-29 19:50:39 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
